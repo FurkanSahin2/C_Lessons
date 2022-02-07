6c2ee4a30077b332e45a6f4912cdb3e5e2c2cf88
M. Furkan Şahin <aliince.000000@gmail.com>
2022-02-07 13:04:04 +0300

    Summer of 2021
    Summer of 2021

--- main59.c
+++ main59.c
@@ -7,7 +7,7 @@ int main() {
 	printf ("Merhaba Bugun 2 Kasim 2021 Sali");
 	system ("color F");
 	
-	// Kullanýlabilecek Renk Kodlarý: 0= Siyah, 1= Mavi, 2= Yesil, 3= Aqua, 4= Kirmizi, 5= Mor, 6= Sari, 7= Beyaz, 8= Gri, 9= Acik Mavi, A= Acik Yesil, B= Acik Aqua, C= Acik Kirmizi, D= Acik Mor, E= Acik Sari, F= Parlak Beyaz                      
+	// Renk Kodlari: 0= Siyah, 1= Mavi, 2= Yesil, 3= Aqua, 4= Kirmizi, 5= Mor, 6= Sari, 7= Beyaz, 8= Gri, 9= Acik Mavi, A= Acik Yesil, B= Acik Aqua, C= Acik Kirmizi, D= Acik Mor, E= Acik Sari, F= Parlak Beyaz                      
 
 
 	return 0;
