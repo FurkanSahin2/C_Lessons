dc55cf711e99c098d347d25ab20e0a30619b70c1
M. Furkan Şahin <aliince.000000@gmail.com>
2024-07-26 20:55:36 +0300

    Create -> File Operations (2)

---
+++ Lesson 55: File Operations (2) - Lesson 58: File Operations (5) - Writing Data to a Text Document/file_operations_2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+	FILE *dosya;
+	dosya=fopen("C:\\Users\\HP\\Desktop\\bilgi.txt","w");
+	
+	// Char'ın içerisine yazdırılacak komutlar tek tırnak içerisine yazılır.
+	
+	putc('a',dosya); // Putc chardan geliyor puts ise stringden geliyor
+	putc('\n',dosya);
+	putc('b',dosya);
+	
+	fclose(dosya);
+	
+	//Ödev 1:
+	
+	FILE*dosya;
+	dosya=fopen("C:\\Users\\HP\\Desktop\\ödev.txt","w");
+
+	fputs("Furkan",dosya);
+	putc('\n',dosya);
+	fputs("Şahin",dosya);
+	putc('\n',dosya);
+	fputs("Yaş: 19",dosya);
+	putc('\n',dosya);
+	fputs("Anadolu Üniversitesi",dosya);
+	
+	return 0;
+}
