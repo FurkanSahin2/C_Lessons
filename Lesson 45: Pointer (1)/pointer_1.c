8adffdb33b295cf2dfd1d1a07211ab3053f2d8f5
M. Furkan Şahin <aliince.000000@gmail.com>
2024-07-26 21:36:03 +0300

    Create -> Pointer (1)

---
+++ Lesson 45: Pointer (1)/pointer_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+	int sayi=10;
+	int sayi2=20;
+	double sayi3=10.25;
+	char kelime='a';
+	
+	printf ("%d\n",sayi);
+	printf ("%d\n",sayi2);
+	printf ("%f\n",sayi3);
+	printf ("%c\n",kelime);
+	
+	printf ("\n\n #Bellek Adresleri# \n\n");
+	
+	// Bellek adreslerini yazdırma da "%x" kullanılır.
+	
+	printf ("%x\n",&sayi);
+	printf ("%x\n",&sayi2);
+	printf ("%x\n",&sayi3);
+	printf ("%x\n",&kelime);
+	
+	
+	return 0;
+}
