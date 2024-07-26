01c5001df97bf175d85bc7d0332606c2e89fce2b
M. Furkan Şahin <aliince.000000@gmail.com>
2024-07-26 22:38:54 +0300

    Create -> Mathematics Functions (5)

---
+++ Lesson 35: Mathematics functions (5) -  Using Sin & Cos/mathematics_functions_5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+	double derece,sonucsin,sonuccos;
+
+	printf ("Dereceyi Girin: ");
+	scanf ("%lf",&derece);
+	
+	sonucsin=sin(derece);
+	printf ("Sinus: %lf\n",sonucsin);
+	
+	sonuccos=cos(derece);
+	printf ("Cosinus: %lf",sonuccos);
+
+ // 5 
+	
+	return 0;
+}
