f93fa8ca817dd7690fc36a7f2135e3d801202fd6
M. Furkan Şahin <aliince.000000@gmail.com>
2024-07-26 22:56:06 +0300

    Create -> Mathematics Functions (3)

---
+++ Lesson 33: Mathematics Functions (3) - Ceil & Floor Usage/mathematics_functions_3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() { 
+	//double'ın klavyeden karşılığı ---> lf  
+	//Bu derste bir sayıyı üste veya alta yuvarlamayı göreceğiz..
+	
+	double sayi,sonuc1,sonuc2;
+	
+	printf ("Degeri girin: ");
+	scanf ("%lf",&sayi);
+	
+	sonuc1=  floor(sayi);
+	printf ("Sonuc: %.f\n",sonuc1);
+	
+	sonuc2= ceil(sayi);
+	printf ("Sonuc: %.f",sonuc2);
+
+ / 3 
+	
+	
+	return 0;
+}
