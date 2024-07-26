dc8e72d0f1f46f2cfce004c78c061d0d2d88a51e
M. Furkan Şahin <aliince.000000@gmail.com>
2024-07-26 22:29:31 +0300

    Create -> Formatted Uses of Strings

---
+++ Lesson 37: Formatted Uses of Strings/formatted_uses_of_strings.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+	char kitap[40];
+	printf ("Ad: ");
+	scanf ("%s",kitap);
+	
+	printf ("%s\n",kitap);
+	
+	printf ("%18s\n",kitap);
+	
+	printf ("%20.9s\n",kitap);
+	
+	printf ("%-20s",kitap);
+	
+
+	
+	return 0;
+}
