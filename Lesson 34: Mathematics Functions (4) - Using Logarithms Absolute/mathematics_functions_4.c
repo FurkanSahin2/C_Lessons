d5c63515df82c466dcb72d196f5ef1eab38d40e6
M. Furkan Şahin <aliince.000000@gmail.com>
2024-07-26 22:47:28 +0300

    Create -> Mathematics Functions (4)

---
+++ Lesson 34: Mathematics Functions (4) - Using Logarithms Absolute/mathematics_functions_4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+	
+	// fabs (float Absolute) = mutlak değer 
+	double sayi,sonuc,sonuc2;
+	
+	printf ("Sayiyi Girin: ");
+	scanf ("%lf",&sayi);
+	
+	sonuc=fabs(sayi);
+	printf ("Sonuc: %.lf\n",sonuc);
+	
+	sonuc2=log(sayi);
+	printf ("Sonuc Logaritma: %.7lf",sonuc2);
+	
+	 / 
+	return 0;
+}
