35b9230f5afa358c5f6591a471ec68bf5123f6c0
M. Furkan Şahin <aliince.000000@gmail.com>
2024-07-27 11:11:07 +0300

    Create -> Loops (3)

---
+++ Lesson 12: Loops (3)/loops_3.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// fibonacci serisinin ilk 10 elemanını listeleyen kodu yazın.
+
+int main() {
+	/*
+	int a,b,c,i;
+	a=1;
+	b=1;
+	
+	printf ("%d\n",a);
+	printf ("%d\n",b);
+	
+	for (i=1;i<=10;i++)
+	{
+		c=a+b;
+		a=b;
+		b=c;
+		printf ("%d\n",c);
+	}
+	
+	int a,b,c,d,i;
+	a=1;
+	b=1;
+	c=1;
+	
+/*	printf ("%d\n",a);
+	printf ("%d\n",b);
+	printf ("%d\n",c);
+	
+	for (i=1;i<=7;i++)
+	{
+		d=a+b+c;
+		a=b;
+		b=c;
+		c=d;
+		printf ("%d\n",d);
+	}
+	*/
+	
+	int a,b,c,d,i;
+	a=1;
+	b=1;
+	c=1;
+	
+	printf ("%d\n",a);
+	printf ("%d\n",b);
+	printf ("%d\n",c);
+	
+	for (i=1;i<=7;i++)
+	{
+		d=a+b+c;
+		a=b;
+		b=c;
+		c=d;
+		printf ("%d\n",d);
+	}
+	
+
+	return 0;
+}
