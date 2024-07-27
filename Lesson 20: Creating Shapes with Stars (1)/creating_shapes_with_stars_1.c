43f942fe1ddf6b68ff4802c779c884084dff572b
M. Furkan Şahin <aliince.000000@gmail.com>
2024-07-27 10:42:31 +0300

    Create -> Creating Shapes with Stars (1)

---
+++ Lesson 20: Creating Shapes with Stars (1)/creating_shapes_with_stars_1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+	/*
+	int i;
+	
+	for(i=1;i<=10;i++)
+	{
+		printf ("*****\n");
+	}
+	
+	
+	int i,j;
+	
+	for (i=1;i<=5;i++)
+	{
+		for (j=1;j<=i;j++)  //her interasyonda başa alıyor. "i" adımlarımı tutarken, "j" ise her adımda kaç yıldız koyacapımı tutacak. "j<=i" nin manası j 1 den baılayıp i'nin değerine kadar gidecek.
+		{
+			printf ("*");
+		}
+			printf ("\n");
+	}
+
+	// Ödev
+	
+	int i,j;
+	
+	for (i=1;i<=5;i++)
+	{
+		printf ("**\n");
+	}
+	*/
+	
+	int i;
+	for (i=1;i<=10;i++)
+	{
+		printf ("*****\n");
+	}
+	
+
+	return 0;
+}
