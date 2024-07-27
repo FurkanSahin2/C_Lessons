0b3b6695b2cfe8974eaf529435316eb31dfec355
M. Furkan Şahin <aliince.000000@gmail.com>
2024-07-27 09:54:07 +0300

    Create -> Answers (3)

---
+++ Lesson 30: Midterm - Final Questions Answers (3)/answers_3.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() { 
+	/*
+	int i,j;
+	
+	for (i=1;i<=5;i++)
+	{
+		for (j=1;j<=i;j++)
+		{
+			printf ("*");
+		}
+		printf ("\n");
+	}
+	
+	
+	 
+	int i,toplam=0;
+	
+	for (i=1;i<=81;i+=4)
+	{
+		toplam= toplam + i;
+	}
+	printf ("Toplam: %d",toplam);
+	
+	
+	
+	int dizi[10]={2,1,0,0,2,1,1,1,2,2};
+	int i,puan=0;
+	
+	for (i=0;i<=10;i++)
+	{
+		if (dizi[i]==2)
+		{
+			puan= puan + 3;
+		}
+		if (dizi[i]==0)
+		{
+			puan= puan + 1;
+		}
+	}
+	if (puan>=12)
+	{
+		printf ("Puaniniz: %d. Bu puan ile kumede kaldiniz.",puan);
+	}
+	if (puan<12)
+	{
+		printf ("Puaniniz: %d. Bu puan ile kumeye dustunuz.",puan);
+	}
+	
+	*/
+	
+
+	return 0;
+}
