5b8292904c7d3e08e69d58f6920f016b05104999
M. Furkan Şahin <aliince.000000@gmail.com>
2024-07-27 11:45:31 +0300

    Create ->Int Variables (1)

---
+++ Lesson 04: Int Variables (1)/int_variables_1.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+  /*
+  
+  int sayi;
+	sayi= 2; 
+	printf ("Girdiginiz Sayi: %d",sayi);
+	
+
+  int sayi1,sayi2,sayi3,toplam;
+  sayi1= 33;
+  sayi2= 44;
+  sayi3= 55;
+  toplam=sayi1+sayi2+sayi3;
+
+  printf ("Toplam Sonuc: %d",toplam);	
+
+  int sayi1,sayi2,toplam,fark,carp,bol;
+  sayi1=50;
+  sayi2=10;
+
+  toplam= sayi1+sayi2;
+  fark= sayi1-sayi2;
+  carp: sayi1*sayi2;
+  bol= sayi1/sayi2;
+
+  printf ("Toplam: %d\n",toplam);
+  printf ("Fark: %d\n",fark);
+  printf ("Carpim: %d\n",carp);
+  printf ("Bolum: %d\n",bol);
+
+
+  //Karenin alan ve çevresi
+  //Deger: Bir kenar
+  //Alan: kenar * kenar
+  //Çevre: kenar * 4
+
+  int kenar, alan, cevre;
+  kenar= 20;
+  alan= kenar*kenar;
+  cevre=kenar*4;
+
+  printf ("Alan: %d\n",alan);
+  printf ("Cevre: %d", cevre);
+
+
+  // Dikdörtgeni alan ve çevresi
+  // Değer: kısa kenar, uzun kenar
+  // Alan: kısa kenar * uzun kenar
+  // Çevre: kısa kenar + uzun kenar*2 
+
+  int kisa, uzun, alan, cevre;
+  kisa= 5;
+  uzun= 10;
+  alan=kisa*uzun;
+  cevre=kisa*2+uzun*2;
+
+  printf ("Alan: %d\n", alan);
+  printf ("Cevre: %d", cevre);
+
+
+  int bagaj,el,fark1,fark2,para;
+  bagaj= 18;
+  el= 9;
+  fark1= bagaj-15;
+  fark2= el-8;
+  para= (fark1+fark2)*5;
+  //or: para= ((bagaj+el)-23)*5;
+
+  printf ("Odemeniz Gereken Tutar: %d",para);
+
+
+  int kutu, poset, fark1, fark2,para;
+  kutu= 25;
+  poset= 10;
+  fark1= kutu-20;
+  fark2= poset-5;
+  para= ((kutu+poset-25)*10);
+
+  printf ("Borcunuz: %d",para);
+
+  */
+
+  int sinav1,sinav2,sinav3,proje,ortalama;
+  sinav1= 75;
+  sinav2= 50;
+  sinav3= 100;
+  proje= 100;
+  ortalama= (sinav1+sinav2+sinav3+proje)/4;
+  printf("Ogrencinin Ders Notu: %d",ortalama);
+
+
+
+return 0;
+}
