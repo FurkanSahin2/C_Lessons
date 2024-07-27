a958653278b4b39848f3a50b782285227c590453
M. Furkan Şahin <aliince.000000@gmail.com>
2024-07-27 10:53:22 +0300

    Create -> Arrays (1)

---
+++ Lesson 17: Arrays (1)/arrays_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	/*
+	int tekRakamlar[]={1,3,5,7,9};
+	printf ("%d",tekRakamlar[2]);
+	
+	
+	char sehir[5]={'a','n','k','a','r','a'};
+	printf ("%s",sehir);
+	*/
+	
+	int sayilar[4];
+	sayilar[0]= 778;
+	sayilar[1]= 456;
+	sayilar[2]= 657;
+	sayilar[3]= 343;
+	sayilar[4]= 678;
+	printf ("%d",sayilar[3]);
+
+	
+	return 0;
+}
