f868d88384199ec8073da64292a5f0ee5926c361
M. Furkan Şahin <aliince.000000@gmail.com>
2024-07-27 11:49:02 +0300

    Create ->Scanf Function

---
+++ Lesson 03: Scanf Function/scanf_function.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {  
+
+    /*  
+
+    char kitapad[20];
+    printf ("Kitabin Adini Giriniz: ");
+    scanf ("%s",kitapad);
+    printf ("Girmis Oldugunuz Kitap: %s",kitapad);
+ 
+   
+   
+     char Ad[10], Soyad[10], Yas[2], Sehir[13], Meslek[10];
+     
+     printf ("Adiniz: ");
+     scanf ("%s",Ad);
+     
+     printf ("Soyadiniz: ");
+     scanf ("%s",Soyad);
+     
+     printf ("Yasiniz: ");
+     scanf("%s",Yas),
+     
+     printf ("Sehriniz: ");
+     scanf ("%s",Sehir);
+     
+     printf ("Mesleginiz: ");
+     scanf ("%s",Meslek);
+     
+     printf ("\n");
+     
+     printf ("Adi: %s\nSoyadi: %s\n",Ad,Soyad);
+     printf ("Yasi: %s\n", Yas);
+     printf ("Sehri: %s\n",Sehir);
+     printf ("Meslegi: %s\n",Meslek);
+    
+     
+     
+     char Kitapad[20], Yazar[20], Sayfa[3], Basimtarihi[4], Basimevi[10], Tur[10];
+
+     printf ("Kitap Adi: ");
+     scanf ("%s",Kitapad);
+     
+     printf ("Yazar: ");
+     scanf ("%s",Yazar);
+     
+     printf ("Sayfa: ");
+     scanf ("%s",Sayfa);
+     
+     printf("Basim Tarihi: ");
+     scanf ("%s",Basimtarihi);
+     
+     printf ("Basim Evi: ");
+     scanf ("%s",Basimevi);
+     
+     printf ("Tur: ");
+     scanf ("%s",Tur);
+     
+     printf ("\n\n");
+     
+     printf ("Girmis Oldugunuz Kitabin Adi: %s\nGirmis Oldugunuz Kitabin Yazari: %s\nGirmis Oldugunuz Kitabin Sayfa Sayisi: %s\n",Kitapad,Yazar,Sayfa);
+	   printf ("Girmis Oldugunuz Kitabin Basim Tarihi: %s\nGirmis Oldugunuz Kitabin Basim Evi: %s\n",Basimtarihi,Basimevi);
+	   printf ("Girmis Oldugunuz Kitabin Turu: %s",Tur);
+    
+     */
+     
+	   char Ad[10], Soyad[10], Yas[2], Sehir[13], Meslek[20];
+	 
+	   printf ("Adinizi Giriniz: ");
+	   scanf ("%s", Ad);
+	 
+	   printf ("Soyadinizi Giriniz: ");
+	   scanf ("%s", Soyad);
+	 
+	   printf ("Yasinizi Giriniz: ");
+	   scanf ("%s", Yas);
+	 
+	   printf ("Yasadiginiz Sehri Giriniz: ");
+	   scanf ("%s", Sehir);
+	 
+	   printf ("Mesleginizi Giriniz: ");
+	   scanf ("%s", Meslek);
+	 
+	   printf ("Girmis Oldugunuz Ad: %s\nGirmis Oldugunuz Soyad: %s\n", Ad, Soyad);
+	   printf ("Girmis Oldugunuz Yas: %s\n", Yas);
+	   printf ("Girmis Oldugunuz Sehir: %s\n", Sehir);
+	   printf ("Girmis Oldugunuz Meslek: %s\n", Meslek);
+	 
+	 
+
+	   return 0;
+}
